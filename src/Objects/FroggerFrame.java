ca57b9619c119cd29e0930a65bae526df32cda37
JohannAtGithub <johannschoene@t-online.de>
2018-05-22 13:36:54 +0200

    main game exitable by ESC key

--- src/Objects/FroggerFrame.java
+++ src/Objects/FroggerFrame.java
@@ -2,11 +2,26 @@ package Objects;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class FroggerFrame extends JFrame {
 
     public FroggerFrame() {
         super("Frogger 2D");
+
+        //Game should be able to close by ESC key
+        InputMap im = getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap am = getRootPane().getActionMap();
+
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "cancel");
+        am.put("cancel", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
         setResizable(false);
         setSize(FroggerPanel.WIDTH, FroggerPanel.HEIGHT);
         setLocationRelativeTo(null);
